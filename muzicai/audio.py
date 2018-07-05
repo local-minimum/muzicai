b3b7ab3aa0ad87bfe4cfd8724109c0626adec370
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-07-05 21:56:12 +0200

    Audio Processings

--- muzicai/audio.py
+++ muzicai/audio.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
-from subprocess import Popen, PIPE
+from subprocess import run
 from io import BytesIO
 import soundfile as sf
 import attr
+import os
+import numpy as np
+from tempfile import TemporaryDirectory
+
+Audio = attr.make_class('Audio', ['data', 'bitrate'])
 
 
-Audio = attr.make_class('Audio', ['bitrate', 'data'])
-
-
-def mp3towav(path):
-    with Popen(['mpg123', '-q', '-w-', path], stdout=PIPE) as proc:
-        proc.wait()
-        return BytesIO(proc.stdout.read())
+def mp3towav(mp3, wav):
+    args = ['mpg123', '-q', '-w', wav, mp3]
+    run(args)
 
 
 
 def get_audio(path):
-    if not path.endswith('.wav'):
-        data = mp3mp3towav(path)
-        return Audio(*sf.read(data))
-    return Audio(*sf.read(path))
+    try:
+        a = Audio(*sf.read(path))
+    except RuntimeError:
+        with TemporaryDirectory() as tmpdir:
+            wav = os.path.join(tmpdir, os.path.basename(path))
+            mp3towav(path, wav)
+            a = Audio(*sf.read(wav))
+    return a
+
+
+def mono_as_sterio(audio):
+    if audio.shape[1] == 1:
+        return Audio(
+            np.vstack((audio.data[:, 0], audio.data[:, 0])).T,
+            audio.bitrate,
+        )
+    return audio
